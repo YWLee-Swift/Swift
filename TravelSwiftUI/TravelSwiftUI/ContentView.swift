990d09583bc29689b0acf3b0f92da120e49c112e
이영운 <lywzz6342@gmail.com>
2021-01-04 13:55:34 +0900

    Section 01: NavigationView, ScrollView, Stack

--- TravelSwiftUI/TravelSwiftUI/ContentView.swift
+++ TravelSwiftUI/TravelSwiftUI/ContentView.swift
@@ -9,13 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        NavigationView {
+            ScrollView {
+                //Show Code Actions -> Extract SubView
+                DiscoverCategoriesView()
+            
+            }.navigationTitle("Discover")
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct DiscoverCategoriesView: View {
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 8) {
+                ForEach(0..<10, id: \.self) { num in
+                    
+                    VStack(spacing: 4) {
+                        Spacer()
+                            .frame(width: 50, height: 50)
+                            .background(Color.gray)
+                            .cornerRadius(.infinity)
+                            .shadow(color: .gray, radius: 4, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: 2)
+                        
+                        Text("Art")
+                            .font(.system(size: 12, weight: .semibold))
+                    }
+                }
+                
+            }.padding(.horizontal)
+        }
     }
 }
