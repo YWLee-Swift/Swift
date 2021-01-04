4efa4a450533745504bd1089b04aa4ee7201feb3
YWLee-Swift <76669442+YWLee-Swift@users.noreply.github.com>
2021-01-04 10:13:06 +0900

    Add files via upload

---
+++ TravelSwiftUI/TravelSwiftUI/TravelSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelSwiftUIApp.swift
+//  TravelSwiftUI
+//
+//  Created by 이영운 on 2021/01/04.
+//
+
+import SwiftUI
+
+@main
+struct TravelSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
