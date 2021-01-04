59d9b87e046b3ca2c0040d39432724878780740f
이영운 <lywzz6342@gmail.com>
2021-01-04 18:10:43 +0900

    ForEach, Hashables, SFImage

--- TravelSwiftUI/TravelSwiftUI/ContentView.swift
+++ TravelSwiftUI/TravelSwiftUI/ContentView.swift
@@ -14,11 +14,102 @@ struct ContentView: View {
             ScrollView {
                 //Show Code Actions -> Extract SubView
                 DiscoverCategoriesView()
+                
+                PopularDestinationsView()
+                
+                PopularRestaurantsView()
+                
+                TrendingCreatorsView()
             
             }.navigationTitle("Discover")
         }
     }
 }
+
+struct PopularDestinationsView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Popular destinations")
+                Spacer()
+                Text("See all")
+            }.padding(.horizontal)
+            .padding(.top)
+            
+            ScrollView(.horizontal) {
+                HStack(spacing: 8) {
+                    ForEach(0..<10, id: \.self) { num in
+                        Spacer()
+                            .frame(width: 125, height: 150)
+                            .background(Color.gray)
+                            .cornerRadius(5.0)
+                            .shadow(color: .gray, radius: 4, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: 2)
+                            .padding(.bottom)
+                    }
+                }.padding(.horizontal)
+            }
+            
+        }
+    }
+}
+
+struct PopularRestaurantsView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Popular places to eat")
+                Spacer()
+                Text("See all")
+            }.padding(.horizontal)
+            .padding(.top)
+            
+            ScrollView(.horizontal) {
+                HStack(spacing: 8) {
+                    ForEach(0..<10, id: \.self) { num in
+                        Spacer()
+                            .frame(width: 200, height: 64)
+                            .background(Color.gray)
+                            .cornerRadius(5.0)
+                            .shadow(color: .gray, radius: 4, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: 2)
+                            .padding(.bottom)
+                    }
+                }.padding(.horizontal)
+            }
+            
+        }
+    }
+}
+
+struct TrendingCreatorsView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Trending Creators")
+                Spacer()
+                Text("See all")
+            }.padding(.horizontal)
+            .padding(.top)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 8) {
+                    ForEach(0..<5, id: \.self) { num in
+                        
+                        Spacer()
+                            .frame(width: 50, height: 50)
+                            .background(Color.gray)
+                            .cornerRadius(.infinity)
+                            .shadow(color: .gray, radius: 4, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: 2)
+                            .padding(.bottom)
+                    }
+                    
+                }.padding(.horizontal)
+            }
+        }
+        
+        
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
@@ -26,22 +117,37 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
+
+struct Category: Hashable {
+    let name, imageName: String
+}
+
 struct DiscoverCategoriesView: View {
+    
+    let categories: [Category] = [
+        .init(name: "Art", imageName: "paintpalette.fill"),
+        .init(name: "Sport", imageName: "sportscourt.fill"),
+        .init(name: "Live Events", imageName: "music.mic"),
+        .init(name: "Food", imageName: "sportscourt.fill"),
+        .init(name: "History", imageName: "sportscourt.fill"),
+    ]
+    
     var body: some View {
         ScrollView(.horizontal, showsIndicators: false) {
-            HStack(spacing: 8) {
-                ForEach(0..<10, id: \.self) { num in
-                    
-                    VStack(spacing: 4) {
-                        Spacer()
-                            .frame(width: 50, height: 50)
+            HStack(alignment: .top, spacing: 14) {
+                ForEach(categories, id: \.self) { category in
+                    VStack(spacing: 8) {
+                        Image(systemName: category.imageName)
+                            .font(.system(size: 20)) // 이미지 크기 조절
+                            .foregroundColor(.white)
+                            .frame(width: 64, height: 64)
                             .background(Color.gray)
-                            .cornerRadius(.infinity)
+                            .cornerRadius(32)
                             .shadow(color: .gray, radius: 4, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: 2)
-                        
-                        Text("Art")
+                        Text(category.name)
                             .font(.system(size: 12, weight: .semibold))
-                    }
+                            .multilineTextAlignment(.center)
+                    }.frame(width: 68)
                 }
                 
             }.padding(.horizontal)
