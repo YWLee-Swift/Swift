b7d03e7b20097b3f21eca1752ff619e399a75fed
이영운 <lywzz6342@gmail.com>
2021-01-05 15:33:07 +0900

    Section 03: Image Sizing

--- TravelSwiftUI/TravelSwiftUI/ContentView.swift
+++ TravelSwiftUI/TravelSwiftUI/ContentView.swift
@@ -26,7 +26,19 @@ struct ContentView: View {
     }
 }
 
+struct Destination: Hashable {
+    let name, country, imageName: String
+}
+
 struct PopularDestinationsView: View {
+    
+    let destinations: [Destination] = [
+        .init(name: "Paris", country: "France", imageName: "eiffelTower"),
+        .init(name: "Tokyo", country: "Japan", imageName: "japan"),
+        .init(name: "New York", country: "US", imageName: "newYork")
+    
+    ]
+    
     var body: some View {
         VStack {
             HStack {
@@ -38,10 +50,30 @@ struct PopularDestinationsView: View {
             
             ScrollView(.horizontal) {
                 HStack(spacing: 8) {
-                    ForEach(0..<10, id: \.self) { num in
-                        Spacer()
-                            .frame(width: 125, height: 150)
-                            .background(Color.gray)
+                    ForEach(destinations, id: \.self) { destination in
+                        VStack(alignment: .leading, spacing: 0) {
+                            
+                            Image(destination.imageName)
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 125, height: 125)
+                                .cornerRadius(4)
+                                .padding(.horizontal, 6)
+                                .padding(.vertical, 6)
+
+                            Text(destination.name)
+                                .font(.system(size: 12, weight: .semibold))
+                                .padding(.horizontal, 12)
+                            
+                            Text(destination.country)
+                                .font(.system(size: 12, weight: .semibold))
+                                .foregroundColor(.gray)
+                                .padding(.horizontal, 12)
+                                .padding(.bottom, 8)
+                            
+                        }
+                            //.frame(width: 125)
+                            .background(Color(.init(white: 0.9, alpha: 1)))
                             .cornerRadius(5.0)
                             .shadow(color: .gray, radius: 4, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: 2)
                             .padding(.bottom)
